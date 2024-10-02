bcdb9fb3a07969aacc07d86334c880ed9618728b
sourav <souravkrishna358@gmail.com>
2024-10-02 11:34:37 +0530

    m2

---
+++ maximumandMinimum.c
@@ -0,0 +1,11 @@
+/*Maximum and Minium in an array*/
+#include<stdio.h>
+int main(){
+int n,max,min,a[100],i;
+printf("Enter the limmit:");
+scanf("%d",&n);
+printf("Enter the elements:");
+for(i=0;i<n;i++){
+
+}
+}
